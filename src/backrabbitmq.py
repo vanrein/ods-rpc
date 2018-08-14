67ca41b1e9d6abbd62d3309a9ef2c8359d4e451a
Rick van Rein <rick@openfortress.nl>
2018-08-14 10:22:49 +0200

    Fix of exception class

--- src/backrabbitmq.py
+++ src/backrabbitmq.py
@@ -169,8 +169,8 @@ def process_cluster_msg (chan, msg, props, body):
 			#TODO# Check timestamp
 			try:
 				go4it = os.stat (flag_path).st_mtime < time_str
-			except IOError, ioe:
-				go4it = ioe.errno == 2
+			except OSError, ose:
+				go4it = ose.errno == 2
 			if go4it:
 				open (flag_path, 'w').write (value)
 				log_debug ('Set RPC flag', zone_flag, 'to', value)
